5bb6a5227667fd9b26f0caf17e1b21d99934d933
Iyad Ahmad <iyad.ahmad@gmail.com>
2021-08-30 16:56:21 +0300

    add permissions

---
+++ backend/api/article/controllers/article.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+  async hello(ctx) {
+    return "Hello yo";
+  },
+};

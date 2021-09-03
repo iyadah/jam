74bf9ec793617f8f587f7e60ab64a30fe294c8c8
Iyad Ahmad <iyad.ahmad@gmail.com>
2021-09-03 11:50:07 +0300

    add contact form

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -13,7 +13,13 @@ module.exports = {
       options: {
         apiURL: `http://localhost:1337`,
         queryLimit: 1000, // Defaults to 100
-        collectionTypes: [`article`, `product`, `category`, `variant`],
+        collectionTypes: [
+          `article`,
+          `product`,
+          `category`,
+          `variant`,
+          `contact`,
+        ],
       },
     },
     {
